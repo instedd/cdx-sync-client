fadfb365e2dc9b9c9bbe8c65ac2a38d89ee342c9
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-01-13 17:57:07 -0300

    Cosmetic changes

--- src/main/groovy/org/instedd/cdx/app/Main.groovy
+++ src/main/groovy/org/instedd/cdx/app/Main.groovy
@@ -1,67 +1,67 @@
-package org.instedd.cdx.app;
+package org.instedd.cdx.app
 
-import static org.instedd.sync4j.util.Exceptions.interruptable;
+import static org.instedd.sync4j.util.Exceptions.interruptable
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.Scanner;
+import java.io.FileInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.util.Properties
+import java.util.Scanner
 
-import org.instedd.sync4j.Settings;
-import org.instedd.sync4j.app.RSyncApplication;
-import org.instedd.sync4j.watcher.RsyncWatchListener.SyncMode;
+import org.instedd.sync4j.Settings
+import org.instedd.sync4j.app.RSyncApplication
+import org.instedd.sync4j.watcher.RsyncWatchListener.SyncMode
 
 public class Main {
-  public static void main(String[] args) throws IOException, InterruptedException {
+  public static void main(String[] args) {
     if (args.length != 1) {
-      System.out.println("Usage: cdxsync <properties filename>");
-      System.exit(1);
+      println("Usage: cdxsync <properties filename>")
+      System.exit(1)
     }
-    def propertiesFilename = args[0];
-    def properties = properties(propertiesFilename);
+    def propertiesFilename = args[0]
+    def properties = properties(propertiesFilename)
 
-    def settings = Settings.fromProperties(properties);
-    System.out.printf("\n\n** Settings are %s **\n\n", settings);
+    def settings = Settings.fromProperties(properties)
+    printf("\n\n** Settings are %s **\n\n", settings)
 
-    def appName = properties.getProperty("app.name");
-    def appIcon = Main.class.getResource(properties.getProperty("app.icon"));
+    def appName = properties.getProperty("app.name")
+    def appIcon = Main.class.getResource(properties.getProperty("app.icon"))
 
-    def appMode = SyncMode.valueOf(properties.getProperty("app.mode").toUpperCase());
+    def appMode = SyncMode.valueOf(properties.getProperty("app.mode").toUpperCase())
 
-    def app = new RSyncApplication(settings, appName, appIcon, appMode);
-    stopOnExit(app);
-    app.start();
+    def app = new RSyncApplication(settings, appName, appIcon, appMode)
+    stopOnExit(app)
+    app.start()
 
-    System.out.printf("\n\n** Now go and create or edit some files on %s **\n\n", settings.localOutboxDir);
-    loop(app);
+    printf("\n\n** Now go and create or edit some files on %s **\n\n", settings.localOutboxDir)
+    loop(app)
 
   }
 
-  protected static void stopOnExit(final RSyncApplication app) {
+  protected static stopOnExit(RSyncApplication app) {
     Runtime.getRuntime().addShutdownHook(new Thread({ ->
       try {
-        interruptable(app.&stop);
+        interruptable(app.&stop)
       } finally {
-        System.out.println("bye!");
+        println("bye!")
       }
-    }));
+    }))
   }
 
-  protected static void loop(RSyncApplication app) {
-    System.out.print("\n\n** Type bye to stop app, or stop it from the system tray **\n\n");
-    Scanner scanner = new Scanner(System.in);
+  protected static loop(RSyncApplication app) {
+    print("\n\n** Type bye to stop app, or stop it from the system tray **\n\n")
+    def scanner = new Scanner(System.in)
     while (scanner.hasNextLine() && app.isRunning()) {
-      if (scanner.nextLine().equals("bye"))
-        break;
+      if (scanner.nextLine() == "bye")
+        break
     }
-    System.exit(0);
+    System.exit(0)
   }
 
   protected static properties(String propertiesFilename) {
     def properties = new Properties()
     new File(propertiesFilename).withInputStream { properties.load(it) }
-    properties;
+    properties
   }
 
 }
