b82f47a04153e341273aa4f9a8231488e0fdf9ed
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-01-15 15:54:10 -0300

    Format

--- src/main/groovy/org/instedd/cdx/app/Credentials.groovy
+++ src/main/groovy/org/instedd/cdx/app/Credentials.groovy
@@ -13,56 +13,54 @@ import org.instedd.sync4j.util.Processes.Exit;
 
 class Credentials {
 
-	static Logger logger = Logger.getLogger(Credentials.name)
+  static Logger logger = Logger.getLogger(Credentials.name)
 
-	File privateKey
-	File publicKey
+  File privateKey
+  File publicKey
 
-	Credentials(File privateKey, File publicKey) {
-		assert publicKey
-		assert publicKey.exists() && publicKey.isFile(), "Invalid public key file: ${publicKey}"
+  Credentials(File privateKey, File publicKey) {
+    assert publicKey
+    assert publicKey.exists() && publicKey.isFile(), "Invalid public key file: ${publicKey}"
 
-		// private key file is not readable, we cannot validate it more than this.
-		assert privateKey
+    // private key file is not readable, we cannot validate it more than this.
+    assert privateKey
 
-		this.privateKey = privateKey
-		this.publicKey = publicKey
-	}
+    this.privateKey = privateKey
+    this.publicKey = publicKey
+  }
 
-	String getPrivateKeyPath() {
-		privateKey.absolutePath
-	}
+  String getPrivateKeyPath() {
+    privateKey.absolutePath
+  }
 
-	String getPublicKeyText() {
-		publicKey.text
-	}
-
-
-	/** Initializes a new pair of SSH keys if necessary. */
-	static Credentials initialize(String keysDirectoryPath, keyName = 'id_rsa') {
-		def dir = new File(keysDirectoryPath)
-		if(!dir.exists()) {
-			logger.info("Creating ${dir.absolutePath} data directory...")
-			dir.mkdirs()
-		}
-
-		assert dir.exists() && dir.isDirectory(), "${keysDirectoryPath} should be a directory"
-
-		def privateKey = new File(dir, keyName)
-		if (!privateKey.exists()) {
-			try {
-				logger.info("Generating a new pair of SSH keys [${privateKey.absolutePath}]")
-				String emptyPassphrase = SystemUtils.IS_OS_WINDOWS ? "\"\"" : "" // windows ignores the argument if it's the empty string instead of passing an empty argument
-				ProcessBuilder command = new ProcessBuilder("ssh-keygen", "-t", "rsa", "-N", emptyPassphrase, "-f", privateKey.path)
-                Exit exit = Processes.run(command)
-                logger.info("Exit value: ${exit.value} Stdout: ${exit.stdout} Stderr: ${exit.stderr}")
-			} catch (Exception e) {
-				logger.severe("A problem occurred while generating ssh keys: ${e}")
-			}
-		}
-
-		new Credentials(privateKey, new File(dir, "${keyName}.pub"));
-	}
+  String getPublicKeyText() {
+    publicKey.text
+  }
 
 
+  /** Initializes a new pair of SSH keys if necessary. */
+  static Credentials initialize(String keysDirectoryPath, keyName = 'id_rsa') {
+    def dir = new File(keysDirectoryPath)
+    if(!dir.exists()) {
+      logger.info("Creating ${dir.absolutePath} data directory...")
+      dir.mkdirs()
+    }
+
+    assert dir.exists() && dir.isDirectory(), "${keysDirectoryPath} should be a directory"
+
+    def privateKey = new File(dir, keyName)
+    if (!privateKey.exists()) {
+      try {
+        logger.info("Generating a new pair of SSH keys [${privateKey.absolutePath}]")
+        String emptyPassphrase = SystemUtils.IS_OS_WINDOWS ? "\"\"" : "" // windows ignores the argument if it's the empty string instead of passing an empty argument
+        ProcessBuilder command = new ProcessBuilder("ssh-keygen", "-t", "rsa", "-N", emptyPassphrase, "-f", privateKey.path)
+        Exit exit = Processes.run(command)
+        logger.info("Exit value: ${exit.value} Stdout: ${exit.stdout} Stderr: ${exit.stderr}")
+      } catch (Exception e) {
+        logger.severe("A problem occurred while generating ssh keys: ${e}")
+      }
+    }
+
+    new Credentials(privateKey, new File(dir, "${keyName}.pub"));
+  }
 }
