5c9b527a0b155d014d89d9ff149adee7522e7ab4
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-01-21 13:33:11 -0300

    Adding file choosers

--- src/main/java/org/instedd/cdx/app/UserSettingsPrompt.java
+++ src/main/java/org/instedd/cdx/app/UserSettingsPrompt.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
@@ -17,6 +18,7 @@ import java.util.Map;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.io.File;
 
 public class UserSettingsPrompt extends JDialog {
 
@@ -51,9 +53,9 @@ public class UserSettingsPrompt extends JDialog {
     contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
     getContentPane().add(contentPanel, BorderLayout.CENTER);
     GridBagLayout gbl_contentPanel = new GridBagLayout();
-    gbl_contentPanel.columnWidths = new int[]{119, 286, 0};
+    gbl_contentPanel.columnWidths = new int[]{119, 286, 0, 0};
     gbl_contentPanel.rowHeights = new int[]{16, 0, 0, 28, 28, 28, 33, 0};
-    gbl_contentPanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+    gbl_contentPanel.columnWeights = new double[]{1.0, 1.0, 0.0, Double.MIN_VALUE};
     gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
     contentPanel.setLayout(gbl_contentPanel);
 
@@ -75,7 +77,8 @@ public class UserSettingsPrompt extends JDialog {
 
             authTokenTextBox = new JTextField();
             GridBagConstraints gbc_authTokenTextBox = new GridBagConstraints();
-            gbc_authTokenTextBox.insets = new Insets(0, 0, 5, 0);
+            gbc_authTokenTextBox.gridwidth = 2;
+            gbc_authTokenTextBox.insets = new Insets(0, 0, 5, 5);
             gbc_authTokenTextBox.fill = GridBagConstraints.HORIZONTAL;
             gbc_authTokenTextBox.gridx = 1;
             gbc_authTokenTextBox.gridy = 1;
@@ -91,9 +94,9 @@ public class UserSettingsPrompt extends JDialog {
             contentPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
 
             authServerUrlTextBox = new JTextField();
-            authServerUrlTextBox.setText("http://localhost:3000");
             GridBagConstraints gbc_authServerUrlTextBox = new GridBagConstraints();
-            gbc_authServerUrlTextBox.insets = new Insets(0, 0, 5, 0);
+            gbc_authServerUrlTextBox.gridwidth = 2;
+            gbc_authServerUrlTextBox.insets = new Insets(0, 0, 5, 5);
             gbc_authServerUrlTextBox.fill = GridBagConstraints.HORIZONTAL;
             gbc_authServerUrlTextBox.gridx = 1;
             gbc_authServerUrlTextBox.gridy = 2;
@@ -113,10 +116,22 @@ public class UserSettingsPrompt extends JDialog {
         GridBagConstraints gbc_remoteKeyTextField = new GridBagConstraints();
         gbc_remoteKeyTextField.anchor = GridBagConstraints.NORTH;
         gbc_remoteKeyTextField.fill = GridBagConstraints.HORIZONTAL;
-        gbc_remoteKeyTextField.insets = new Insets(0, 0, 5, 0);
+        gbc_remoteKeyTextField.insets = new Insets(0, 0, 5, 5);
         gbc_remoteKeyTextField.gridx = 1;
         gbc_remoteKeyTextField.gridy = 3;
         contentPanel.add(remoteKeyTextField, gbc_remoteKeyTextField);
+
+                JButton remoteKeyChooseButton = new JButton("Choose");
+                remoteKeyChooseButton.addActionListener(new ActionListener() {
+                  public void actionPerformed(ActionEvent e) {
+                    fillTextFromFileChooser(remoteKeyTextField);
+                  }
+                });
+                GridBagConstraints gbc_remoteKeyChooseButton = new GridBagConstraints();
+                gbc_remoteKeyChooseButton.insets = new Insets(0, 0, 5, 0);
+                gbc_remoteKeyChooseButton.gridx = 2;
+                gbc_remoteKeyChooseButton.gridy = 3;
+                contentPanel.add(remoteKeyChooseButton, gbc_remoteKeyChooseButton);
 
                 JLabel lblKnownHostsPath = new JLabel("Known Hosts Path:");
                 GridBagConstraints gbc_lblKnownHostsPath = new GridBagConstraints();
@@ -130,10 +145,22 @@ public class UserSettingsPrompt extends JDialog {
             knownHostsPathTextField.setColumns(10);
             GridBagConstraints gbc_knownHostsPathTextField = new GridBagConstraints();
             gbc_knownHostsPathTextField.fill = GridBagConstraints.BOTH;
-            gbc_knownHostsPathTextField.insets = new Insets(0, 0, 5, 0);
+            gbc_knownHostsPathTextField.insets = new Insets(0, 0, 5, 5);
             gbc_knownHostsPathTextField.gridx = 1;
             gbc_knownHostsPathTextField.gridy = 4;
             contentPanel.add(knownHostsPathTextField, gbc_knownHostsPathTextField);
+
+            JButton knownHostsPathChooseButton = new JButton("Choose");
+            knownHostsPathChooseButton.addActionListener(new ActionListener() {
+              public void actionPerformed(ActionEvent e) {
+                fillTextFromFileChooser(knownHostsPathTextField);
+              }
+            });
+            GridBagConstraints gbc_knownHostsPathChooseButton = new GridBagConstraints();
+            gbc_knownHostsPathChooseButton.insets = new Insets(0, 0, 5, 0);
+            gbc_knownHostsPathChooseButton.gridx = 2;
+            gbc_knownHostsPathChooseButton.gridy = 4;
+            contentPanel.add(knownHostsPathChooseButton, gbc_knownHostsPathChooseButton);
 
             JLabel lblOutboxPath = new JLabel("Inbox Path:");
             GridBagConstraints gbc_lblOutboxPath = new GridBagConstraints();
@@ -147,10 +174,22 @@ public class UserSettingsPrompt extends JDialog {
             inboxPathTextField.setColumns(10);
             GridBagConstraints gbc_inboxPathTextField = new GridBagConstraints();
             gbc_inboxPathTextField.fill = GridBagConstraints.BOTH;
-            gbc_inboxPathTextField.insets = new Insets(0, 0, 5, 0);
+            gbc_inboxPathTextField.insets = new Insets(0, 0, 5, 5);
             gbc_inboxPathTextField.gridx = 1;
             gbc_inboxPathTextField.gridy = 5;
             contentPanel.add(inboxPathTextField, gbc_inboxPathTextField);
+
+            JButton inboxPathChooseButton = new JButton("Choose");
+            inboxPathChooseButton.addActionListener(new ActionListener() {
+              public void actionPerformed(ActionEvent e) {
+                fillTextFromFileChooser(inboxPathTextField);
+              }
+            });
+            GridBagConstraints gbc_inboxPathChooseButton = new GridBagConstraints();
+            gbc_inboxPathChooseButton.insets = new Insets(0, 0, 5, 0);
+            gbc_inboxPathChooseButton.gridx = 2;
+            gbc_inboxPathChooseButton.gridy = 5;
+            contentPanel.add(inboxPathChooseButton, gbc_inboxPathChooseButton);
 
             JLabel label = new JLabel("Outbox Path:");
             GridBagConstraints gbc_label = new GridBagConstraints();
@@ -163,10 +202,22 @@ public class UserSettingsPrompt extends JDialog {
         outboxPathTextField = new JTextField();
         outboxPathTextField.setColumns(10);
         GridBagConstraints gbc_outboxPathTextField = new GridBagConstraints();
+        gbc_outboxPathTextField.insets = new Insets(0, 0, 0, 5);
         gbc_outboxPathTextField.fill = GridBagConstraints.BOTH;
         gbc_outboxPathTextField.gridx = 1;
         gbc_outboxPathTextField.gridy = 6;
         contentPanel.add(outboxPathTextField, gbc_outboxPathTextField);
+
+        JButton outboxPathChooseButton = new JButton("Choose");
+        outboxPathChooseButton.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            fillTextFromFileChooser(outboxPathTextField);
+          }
+        });
+        GridBagConstraints gbc_outboxPathChooseButton = new GridBagConstraints();
+        gbc_outboxPathChooseButton.gridx = 2;
+        gbc_outboxPathChooseButton.gridy = 6;
+        contentPanel.add(outboxPathChooseButton, gbc_outboxPathChooseButton);
     {
       JPanel buttonPane = new JPanel();
       buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -185,12 +236,29 @@ public class UserSettingsPrompt extends JDialog {
     }
   }
 
-  public static Map<String, String> promptForUserSettings() {
+  protected void fillTextFromFileChooser(JTextField textField) {
+    JFileChooser fc = new JFileChooser();
+    int returnVal = fc.showOpenDialog(UserSettingsPrompt.this);
+    if (returnVal == JFileChooser.APPROVE_OPTION) {
+        File file = fc.getSelectedFile();
+        textField.setText(file.getAbsolutePath());
+    }
+  }
+
+  public static Map<String, String> promptForUserSettings(Map<String, String> defaultSettings) {
     UserSettingsPrompt prompt = new UserSettingsPrompt();
+//    prompt.setSettings(defaultSettings);
     prompt.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
     prompt.setVisible(true);
     prompt.dispose();
     return prompt.getSettings();
+  }
+
+  public void setSettings(Map<String, String> settings) {
+      remoteKeyTextField.setText(settings.get("remoteKey"));
+      knownHostsPathTextField.setText(settings.get("knownHostsFilePath"));
+      inboxPathTextField.setText(settings.get("localInboxDir"));
+      outboxPathTextField.setText(settings.get("localOutboxDir"));
   }
 
   public Map<String, String> getSettings() {
