2419245623eb314461d15faf50d722085a5c41a8
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-05-20 17:37:04 -0300

    Remove "inbox" folder from settings (fixes #170)

--- src/main/java/org/instedd/cdx/app/UserSettingsPrompt.java
+++ src/main/java/org/instedd/cdx/app/UserSettingsPrompt.java
@@ -23,26 +23,9 @@ import java.io.File;
 public class UserSettingsPrompt extends JDialog {
 
   private final JPanel contentPanel = new JPanel();
-  private JTextField inboxPathTextField;
   private JTextField outboxPathTextField;
   private JTextField authTokenTextBox;
 
-  /**
-   * Launch the application.
-   */
-  public static void main(String[] args) {
-    try {
-      UserSettingsPrompt dialog = new UserSettingsPrompt();
-      dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-      dialog.setVisible(true);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  /**
-   * Create the dialog.
-   */
   public UserSettingsPrompt() {
     setModal(true);
     setBounds(100, 100, 450, 300);
@@ -56,104 +39,75 @@ public class UserSettingsPrompt extends JDialog {
     gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
     contentPanel.setLayout(gbl_contentPanel);
 
-                JLabel lblNewLabel = new JLabel("Enter your settings");
-                GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-                gbc_lblNewLabel.anchor = GridBagConstraints.NORTHWEST;
-                gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-                gbc_lblNewLabel.gridx = 0;
-                gbc_lblNewLabel.gridy = 0;
-                contentPanel.add(lblNewLabel, gbc_lblNewLabel);
+    JLabel lblNewLabel = new JLabel("Enter your settings");
+    GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+    gbc_lblNewLabel.anchor = GridBagConstraints.NORTHWEST;
+    gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+    gbc_lblNewLabel.gridx = 0;
+    gbc_lblNewLabel.gridwidth = GridBagConstraints.REMAINDER;
+    gbc_lblNewLabel.gridy = 0;
+    contentPanel.add(lblNewLabel, gbc_lblNewLabel);
 
-            JLabel lblNewLabel_2 = new JLabel("Auth Token");
-            GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-            gbc_lblNewLabel_2.fill = GridBagConstraints.HORIZONTAL;
-            gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-            gbc_lblNewLabel_2.gridx = 0;
-            gbc_lblNewLabel_2.gridy = 1;
-            contentPanel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+    JLabel lblNewLabel_2 = new JLabel("Auth Token");
+    GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+    gbc_lblNewLabel_2.fill = GridBagConstraints.HORIZONTAL;
+    gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+    gbc_lblNewLabel_2.gridx = 0;
+    gbc_lblNewLabel_2.gridy = 1;
+    contentPanel.add(lblNewLabel_2, gbc_lblNewLabel_2);
 
-            authTokenTextBox = new JTextField();
-            GridBagConstraints gbc_authTokenTextBox = new GridBagConstraints();
-            gbc_authTokenTextBox.gridwidth = 2;
-            gbc_authTokenTextBox.insets = new Insets(0, 0, 5, 0);
-            gbc_authTokenTextBox.fill = GridBagConstraints.HORIZONTAL;
-            gbc_authTokenTextBox.gridx = 1;
-            gbc_authTokenTextBox.gridy = 1;
-            contentPanel.add(authTokenTextBox, gbc_authTokenTextBox);
-            authTokenTextBox.setColumns(10);
+    authTokenTextBox = new JTextField();
+    GridBagConstraints gbc_authTokenTextBox = new GridBagConstraints();
+    gbc_authTokenTextBox.gridwidth = 2;
+    gbc_authTokenTextBox.insets = new Insets(0, 0, 5, 0);
+    gbc_authTokenTextBox.fill = GridBagConstraints.HORIZONTAL;
+    gbc_authTokenTextBox.gridx = 1;
+    gbc_authTokenTextBox.gridy = 1;
+    contentPanel.add(authTokenTextBox, gbc_authTokenTextBox);
+    authTokenTextBox.setColumns(10);
 
-                        JLabel lblInboxPath = new JLabel("Inbox Path:");
-                        GridBagConstraints gbc_lblInboxPath = new GridBagConstraints();
-                        gbc_lblInboxPath.anchor = GridBagConstraints.WEST;
-                        gbc_lblInboxPath.insets = new Insets(0, 0, 5, 5);
-                        gbc_lblInboxPath.gridx = 0;
-                        gbc_lblInboxPath.gridy = 2;
-                        contentPanel.add(lblInboxPath, gbc_lblInboxPath);
+    JLabel label = new JLabel("Outbox Path:");
+    GridBagConstraints gbc_label = new GridBagConstraints();
+    gbc_label.anchor = GridBagConstraints.WEST;
+    gbc_label.insets = new Insets(0, 0, 5, 5);
+    gbc_label.gridx = 0;
+    gbc_label.gridy = 3;
+    contentPanel.add(label, gbc_label);
 
-                                    inboxPathTextField = new JTextField();
-                                    inboxPathTextField.setColumns(10);
-                                    GridBagConstraints gbc_inboxPathTextField = new GridBagConstraints();
-                                    gbc_inboxPathTextField.fill = GridBagConstraints.BOTH;
-                                    gbc_inboxPathTextField.insets = new Insets(0, 0, 5, 5);
-                                    gbc_inboxPathTextField.gridx = 1;
-                                    gbc_inboxPathTextField.gridy = 2;
-                                    contentPanel.add(inboxPathTextField, gbc_inboxPathTextField);
+    outboxPathTextField = new JTextField();
+    outboxPathTextField.setColumns(10);
+    GridBagConstraints gbc_outboxPathTextField = new GridBagConstraints();
+    gbc_outboxPathTextField.insets = new Insets(0, 0, 5, 5);
+    gbc_outboxPathTextField.fill = GridBagConstraints.BOTH;
+    gbc_outboxPathTextField.gridx = 1;
+    gbc_outboxPathTextField.gridy = 3;
+    contentPanel.add(outboxPathTextField, gbc_outboxPathTextField);
 
-                                    JButton inboxPathChooseButton = new JButton("Choose");
-                                    inboxPathChooseButton.addActionListener(new ActionListener() {
-                                      public void actionPerformed(ActionEvent e) {
-                                        fillTextFromFileChooser(inboxPathTextField);
-                                      }
-                                    });
-                                    GridBagConstraints gbc_inboxPathChooseButton = new GridBagConstraints();
-                                    gbc_inboxPathChooseButton.insets = new Insets(0, 0, 5, 0);
-                                    gbc_inboxPathChooseButton.gridx = 2;
-                                    gbc_inboxPathChooseButton.gridy = 2;
-                                    contentPanel.add(inboxPathChooseButton, gbc_inboxPathChooseButton);
-
-                        JLabel label = new JLabel("Outbox Path:");
-                        GridBagConstraints gbc_label = new GridBagConstraints();
-                        gbc_label.anchor = GridBagConstraints.WEST;
-                        gbc_label.insets = new Insets(0, 0, 5, 5);
-                        gbc_label.gridx = 0;
-                        gbc_label.gridy = 3;
-                        contentPanel.add(label, gbc_label);
-
-                    outboxPathTextField = new JTextField();
-                    outboxPathTextField.setColumns(10);
-                    GridBagConstraints gbc_outboxPathTextField = new GridBagConstraints();
-                    gbc_outboxPathTextField.insets = new Insets(0, 0, 5, 5);
-                    gbc_outboxPathTextField.fill = GridBagConstraints.BOTH;
-                    gbc_outboxPathTextField.gridx = 1;
-                    gbc_outboxPathTextField.gridy = 3;
-                    contentPanel.add(outboxPathTextField, gbc_outboxPathTextField);
-
-                    JButton outboxPathChooseButton = new JButton("Choose");
-                    outboxPathChooseButton.addActionListener(new ActionListener() {
-                      public void actionPerformed(ActionEvent e) {
-                        fillTextFromFileChooser(outboxPathTextField);
-                      }
-                    });
-                    GridBagConstraints gbc_outboxPathChooseButton = new GridBagConstraints();
-                    gbc_outboxPathChooseButton.insets = new Insets(0, 0, 5, 0);
-                    gbc_outboxPathChooseButton.gridx = 2;
-                    gbc_outboxPathChooseButton.gridy = 3;
-                    contentPanel.add(outboxPathChooseButton, gbc_outboxPathChooseButton);
-    {
-      JPanel buttonPane = new JPanel();
-      buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-      getContentPane().add(buttonPane, BorderLayout.SOUTH);
-      {
-        JButton okButton = new JButton("OK");
-        okButton.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent e) {
-            setVisible(false);
-          }
-        });
-        okButton.setActionCommand("OK");
-        buttonPane.add(okButton);
-        getRootPane().setDefaultButton(okButton);
+    JButton outboxPathChooseButton = new JButton("Choose");
+    outboxPathChooseButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        fillTextFromFileChooser(outboxPathTextField);
       }
+    });
+    GridBagConstraints gbc_outboxPathChooseButton = new GridBagConstraints();
+    gbc_outboxPathChooseButton.insets = new Insets(0, 0, 5, 0);
+    gbc_outboxPathChooseButton.gridx = 2;
+    gbc_outboxPathChooseButton.gridy = 3;
+    contentPanel.add(outboxPathChooseButton, gbc_outboxPathChooseButton);
+
+    JPanel buttonPane = new JPanel();
+    buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+    getContentPane().add(buttonPane, BorderLayout.SOUTH);
+    {
+      JButton okButton = new JButton("OK");
+      okButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          setVisible(false);
+        }
+      });
+      okButton.setActionCommand("OK");
+      buttonPane.add(okButton);
+      getRootPane().setDefaultButton(okButton);
     }
   }
 
@@ -179,7 +133,6 @@ public class UserSettingsPrompt extends JDialog {
     return new HashMap<String, String>(){
       {
         put("authToken", authTokenTextBox.getText());
-        put("localInboxDir",inboxPathTextField.getText());
         put("localOutboxDir", outboxPathTextField.getText());
       }
     };
